31714ac414a189b4450e126668d8440315e5748b
Aniket <aniket278dhupad@gmail.com>
2023-11-08 12:09:26 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    a=12;
+    cout<<"Size of int"<<sizeof(a)<<endl;
+    float b;
+    cout<<"Size of float"<<sizeof(b)<<endl;
+    char c;
+    cout<<"Size of char"<<sizeof(c)<<endl;
+    float d;
+    cout<<"Size of float"<<sizeof(d)<<endl;
+    return 0;
+
+
+}
